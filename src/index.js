327d9944ba2a47b48e8d9547a85e82ef924afbe3
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2024-07-26 13:37:31 -0700

    testing homepage

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Homepage from './components/Homepage/Homepage';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

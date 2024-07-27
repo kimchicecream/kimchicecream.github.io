4398ad4a9825242d38863b3503c698eb161bc923
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2024-07-27 10:27:20 -0700

    testing for deploy

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,8 +1,14 @@
+import './Homepage.css';
+
 function Homepage() {
     return (
-        <>
+        <div className="home-container">
             <h1>Homepage</h1>
-        </>
+            <div className='hero'></div>
+            <div className='projects'></div>
+            <div className='tech'></div>
+            <div className='footer'></div>
+        </div>
     )
 }
 
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,7 +3,7 @@ import './Navigation.css';
 function Navigation() {
     return (
         <div className="nav-container">
-            {/* <img src='/'></img> */}
+            <img src='/portfolio_logo.png' />
         </div>
     )
 }

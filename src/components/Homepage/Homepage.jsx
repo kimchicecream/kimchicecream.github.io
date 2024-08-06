d841a477c0ea478dd08043ebfa4117481ed24d98
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2024-08-05 18:11:07 -0700

    rebranded

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -3,8 +3,13 @@ import './Homepage.css';
 function Homepage() {
     return (
         <div className="home-container">
-            <h1>Homepage</h1>
-            <div className='hero'></div>
+            <div className='hero'>
+                <h1>Hi! I'm Alex.</h1>
+                <h2>I love to</h2>
+                <h2 id='build'>build</h2>
+                <h2 id='fix'>fix</h2>
+                <h2 id='create'>create</h2>
+            </div>
             <div className='projects'></div>
             <div className='tech'></div>
             <div className='footer'></div>

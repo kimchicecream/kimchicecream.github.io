335cf5b41cd6553dcd47c8f74381142ff3626074
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2024-08-06 12:54:28 -0700

    tuesday commit

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -4,15 +4,30 @@ function Homepage() {
     return (
         <div className="home-container">
             <div className='hero'>
-                <h1>Hi! I'm Alex.</h1>
-                <h2>I love to</h2>
-                <h2 id='build'>build</h2>
-                <h2 id='fix'>fix</h2>
-                <h2 id='create'>create</h2>
+                <p id='welcome'>Welcome.</p>
+                <p id='paragraph'>
+                    Alex is a full-stack software developer specializing in dynamic applications and UI/UX design.
+                    His keen eye for detail and adaptive coding skills bring creativity and innovation to every project.
+                </p>
             </div>
-            <div className='projects'></div>
+            <div className='projects'>
+                <h3 className='title'>Projects</h3>
+                <div className='project' id='riddym'>
+                    <h2>Riddym</h2>
+                    <img src='../../public/riddym/riddym-homepage.png' alt='riddym' />
+                </div>
+                <div className='project' id='babbl'>
+
+                </div>
+                <div className='project' id='fortbnb'>
+
+                </div>
+            </div>
             <div className='tech'></div>
-            <div className='footer'></div>
+            <div className='footer'>
+                <span id='name'>Alex Go</span>
+                <span id='copyright'><i className="fa-regular fa-copyright"></i> 2024</span>
+            </div>
         </div>
     )
 }

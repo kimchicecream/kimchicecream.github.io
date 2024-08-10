83c2c9c51847196c9989fdc599635ed99f74cccb
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2024-08-10 14:52:02 -0700

    commit for deploy

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -24,6 +24,33 @@ function Homepage() {
         };
     }, []);
 
+    useEffect(() => {
+        const logos = document.querySelectorAll('.tech .logos-container .tooltip img');
+
+        logos.forEach(logo => {
+            logo.addEventListener('mousemove', (e) => {
+                const rect = logo.getBoundingClientRect();
+                const x = e.clientX - (rect.left + rect.width / 2);
+                const y = e.clientY - (rect.top + rect.height / 2);
+
+                // Move the image towards the cursor more precisely
+                logo.style.transform = `translate(${x / 8}px, ${y / 8}px)`;
+            });
+
+            logo.addEventListener('mouseleave', () => {
+                // Reset the image position with a rubber band effect
+                logo.style.transform = 'translate(0, 0)';
+            });
+        });
+
+        return () => {
+            logos.forEach(logo => {
+                logo.removeEventListener('mousemove', null);
+                logo.removeEventListener('mouseleave', null);
+            });
+        };
+    }, []);
+
     return (
         <div className="home-container">
             <div className='hero'>
@@ -35,6 +62,68 @@ function Homepage() {
             </div>
             <div className='tech'>
                 <h3 className='title'>Technologies</h3>
+                <div className='logos-container'>
+                    <div className='tooltip'>
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" alt="JavaScript" />
+                        <span className='tooltiptext'>JavaScript</span>
+                    </div>
+                    <div className='tooltip'>
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg" alt="Python" />
+                        <span className='tooltiptext'>Python</span>
+                    </div>
+                    <div className='tooltip'>
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg" alt="MySQL" />
+                        <span className='tooltiptext'>MySQL</span>
+                    </div>
+                    <div className='tooltip'>
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sequelize/sequelize-original.svg" alt="Sequelize" />
+                        <span className='tooltiptext'>Sequelize</span>
+                    </div>
+                    <div className='tooltip'>
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" alt="CSS3" />
+                        <span className='tooltiptext'>CSS3</span>
+                    </div>
+                    <div className='tooltip'>
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" alt="HTML5" />
+                        <span className='tooltiptext'>HTML5</span>
+                    </div>
+                    <div className='tooltip'>
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/amazonwebservices/amazonwebservices-original-wordmark.svg" alt="AWS" />
+                        <span className='tooltiptext'>AWS</span>
+                    </div>
+                    <div className='tooltip'>
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg" alt="Node.js" />
+                        <span className='tooltiptext'>Node.js</span>
+                    </div>
+                    <div className='tooltip'>
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/redux/redux-original.svg" alt="Redux" />
+                        <span className='tooltiptext'>Redux</span>
+                    </div>
+                    <div className='tooltip'>
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/flask/flask-original.svg" alt="Flask" />
+                        <span className='tooltiptext'>Flask</span>
+                    </div>
+                    <div className='tooltip'>
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg" alt="Git" />
+                        <span className='tooltiptext'>Git</span>
+                    </div>
+                    <div className='tooltip'>
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" alt="React" />
+                        <span className='tooltiptext'>React</span>
+                    </div>
+                    <div className='tooltip'>
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sqlite/sqlite-original.svg" alt="SQLite" />
+                        <span className='tooltiptext'>SQLite</span>
+                    </div>
+                    <div className='tooltip'>
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/photoshop/photoshop-original.svg" alt="Photoshop" />
+                        <span className='tooltiptext'>Photoshop</span>
+                    </div>
+                    <div className='tooltip'>
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/illustrator/illustrator-plain.svg" alt="Illustrator" />
+                        <span className='tooltiptext'>Illustrator</span>
+                    </div>
+                </div>
             </div>
             <div className='projects'>
                 <h3 className='title'>Projects</h3>
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,7 +3,26 @@ import './Navigation.css';
 function Navigation() {
     return (
         <div className="nav-container">
-            <img src='/portfolio_logo.png' alt='Logo'/>
+            <div className='section-one'>
+                <img src='/portfolio_logo.png' alt='Logo'/>
+                <h3 className='about'>About</h3>
+                <h3 className='CV'>CV</h3>
+            </div>
+            <span></span>
+            <div className='section-two'>
+                <p id='title'>PROJECTS</p>
+                <a href="" className='riddym'>Riddym</a>
+                <a href="" className='babbl'>babbl</a>
+                <a href="" className='fortbnb'>Fortbnb</a>
+            </div>
+            <span></span>
+            <div className='section-three'>
+                <p id='title'>CONNECT</p>
+                <a href="mailto:gohalexx@gmail.com" className='email' target="_blank">Email</a>
+                <a href="https://www.linkedin.com/in/alexhgo9/" target="_blank" className='linkedin'>LinkedIn</a>
+                <a href="https://github.com/kimchicecream" target="_blank" className='github'>Github</a>
+                <a href="/Resume.pdf" target="_blank" className='resume'>Resume</a>
+            </div>
         </div>
     )
 }

9bf1e573c75653d8da473c32d003c1fca438db2d
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2024-08-24 16:23:54 -0700

    created new routes in router

--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from './components/Layout.js';
 import Homepage from './components/Homepage';
 import Riddym from "./components/Riddym/index.js";
+import Babbl from './components/Babbl/index.js';
+import Fortbnb from './components/Fortbnb/index.js';
 
 const router = createBrowserRouter([
     {
@@ -14,6 +16,14 @@ const router = createBrowserRouter([
             {
                 path: '/riddym',
                 element: <Riddym />
+            },
+            {
+                path: '/babbl',
+                element: <Babbl />
+            },
+            {
+                path: '/fortbnb',
+                element: <Fortbnb />
             }
         ]
     }

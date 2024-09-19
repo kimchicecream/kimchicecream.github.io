5103d50a16ea73fa4452df8ab404d2327ec22fc6
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2024-09-19 02:09:26 -0700

    added project page layout to all projects

--- src/components/Babbl/Babbl.jsx
+++ src/components/Babbl/Babbl.jsx
@@ -1,8 +1,24 @@
+import { useNavigate } from 'react-router-dom';
 import './Babbl.css';
 
 function Babbl () {
+    const navigate = useNavigate();
+
+    const handleBack = () => {
+        navigate('/');
+    };
+
     return (
-        <></>
+        <div className='babbl-container'>
+            <div className='top'>
+                <div className='back-button' onClick={handleBack}>
+                    <i className="fa-solid fa-arrow-left"></i><span>Back</span>
+                </div>
+            </div>
+            <div className='content'>
+                <h1>Undergoing a design process...</h1>
+            </div>
+        </div>
     )
 }
 
--- src/components/Fortbnb/Fortbnb.jsx
+++ src/components/Fortbnb/Fortbnb.jsx
@@ -1,9 +1,24 @@
+import { useNavigate } from 'react-router-dom';
 import './Fortbnb.css';
 
 function Fortbnb() {
+    const navigate = useNavigate();
+
+    const handleBack = () => {
+        navigate('/');
+    };
 
     return (
-        <></>
+        <div className='fortbnb-container'>
+            <div className='top'>
+                <div className='back-button' onClick={handleBack}>
+                    <i className="fa-solid fa-arrow-left"></i><span>Back</span>
+                </div>
+            </div>
+            <div className='content'>
+                <h1>Undergoing a design process...</h1>
+            </div>
+        </div>
     )
 }
 

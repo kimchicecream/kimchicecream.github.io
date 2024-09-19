32b7fe40f0e93e34b654ad814336aaeca8472493
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2024-09-19 01:52:19 -0700

    create back button in project

--- src/components/Riddym/Riddym.jsx
+++ src/components/Riddym/Riddym.jsx
@@ -1,8 +1,24 @@
+import { useNavigate } from 'react-router-dom';
 import './Riddym.css';
 
 function Riddym() {
+    const navigate = useNavigate();
+
+    const handleBack = () => {
+        navigate('/');
+    }
+
     return (
-        <></>
+        <div className='riddym-container'>
+            <div className='top'>
+                <div className='back-button' onClick={handleBack}>
+                    <i className="fa-solid fa-arrow-left"></i><span>Back</span>
+                </div>
+            </div>
+            <div className='content'>
+                <h1>Under maintenance</h1>
+            </div>
+        </div>
     )
 }
 

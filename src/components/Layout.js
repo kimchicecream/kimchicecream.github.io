8d7dac800236639bb07d2509cd6dff5ebb555c19
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2024-09-19 01:39:06 -0700

    adjusted Layout component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,11 +2,13 @@ import { Outlet, useLocation } from "react-router-dom";
 import Navigation from "./Navigation/Navigation";
 
 function Layout() {
-    const
+    const location = useLocation();
+
+    const isHomepage = location.pathname === '/';
 
     return (
         <>
-            <Navigation />
+            {isHomepage && <Navigation />}
             <Outlet />
         </>
     )

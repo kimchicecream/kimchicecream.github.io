c59f90cbbc2287b6535c829c8413a8ba36525515
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2024-10-05 21:51:01 -0700

    continuing about

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -5,6 +5,7 @@ function AboutPage() {
         <div className='about-page'>
             <h1>About Me</h1>
             <h2>Hi Im Alex.</h2>
+            <h3>This is my about page.</h3>
         </div>
     )
 }

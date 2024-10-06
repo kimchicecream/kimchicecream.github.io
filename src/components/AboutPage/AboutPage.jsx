9b880d90d954b792f9561824c034c6b5647ec7a7
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2024-10-05 21:52:14 -0700

    created coding section

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -6,7 +6,9 @@ function AboutPage() {
             <h1>About Me</h1>
             <h2>Hi Im Alex.</h2>
             <h3>This is my about page.</h3>
-            <div className='hobbies'></div>
+            <div className='hobbies'>
+                <div className='coding'></div>
+            </div>
         </div>
     )
 }

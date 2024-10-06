d0f3c1f8d2b1eff688d8d14a676ab6be6a00cdd7
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2024-10-05 21:51:58 -0700

    created hobbies

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -6,6 +6,7 @@ function AboutPage() {
             <h1>About Me</h1>
             <h2>Hi Im Alex.</h2>
             <h3>This is my about page.</h3>
+            <div className='hobbies'></div>
         </div>
     )
 }

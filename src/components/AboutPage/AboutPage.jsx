4b7323697b0f954a699a27d13673013c1ab8c128
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2024-10-11 10:36:02 -0700

    about page

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -3,13 +3,13 @@ import './AboutPage.css';
 function AboutPage() {
     return (
         <div className='about-page'>
-            <h1>About Me</h1>
-            <h2>Hi Im Alex.</h2>
-            <h3>This is my about page.</h3>
-            <div className='hobbies'>
-                <div className='coding'></div>
-                <div className="formula"></div>
-                <div className="cars"></div>
+            <div className='top'>
+                <div className='back-button' onClick={handleBack}>
+                    <i className="fa-solid fa-arrow-left"></i><span>Back</span>
+                </div>
+            </div>
+            <div className='content'>
+                <h1>Undergoing a design process...</h1>
             </div>
         </div>
     )

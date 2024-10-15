a4bec9979a29ea00104dafd4b377a406562392a9
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2024-10-14 19:08:17 -0700

    changes made to navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,24 +4,11 @@ function Navigation() {
     return (
         <div className="nav-container">
             <div className='section-one'>
-                <img src='/portfolio_logo.png' alt='Logo'/>
+                <div className='image-container'>
+                    <img src='/portfolio_logo.svg' alt='Logo' className='logo'/>
+                </div>
                 <h3 className='about'>About</h3>
             </div>
-            {/* <span></span>
-            <div className='section-two'>
-                <p id='title'>PROJECTS</p>
-                <a href="" className='riddym'>Riddym</a>
-                <a href="" className='babbl'>babbl</a>
-                <a href="" className='fortbnb'>Fortbnb</a>
-            </div>
-            <span></span>
-            <div className='section-three'>
-                <p id='title'>CONNECT</p>
-                <a href="mailto:gohalexx@gmail.com" className='email' target="_blank">Email</a>
-                <a href="https://www.linkedin.com/in/alexhgo9/" target="_blank" className='linkedin'>LinkedIn</a>
-                <a href="https://github.com/kimchicecream" target="_blank" className='github'>Github</a>
-                <a href="/Resume.pdf" target="_blank" className='resume'>Resume</a>
-            </div> */}
         </div>
     )
 }

f57fad7f633d7b115ab1acd6d9ae5bac5924982f
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2024-10-14 19:23:58 -0700

    changes made to navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,13 +1,23 @@
+import { useNavigate } from 'react-router-dom';
 import './Navigation.css';
 
 function Navigation() {
+    const navigate = useNavigate();
+
+    const handleAboutClick = () => {
+        navigate('/about');
+    };
+
     return (
         <div className="nav-container">
             <div className='section-one'>
                 <div className='image-container'>
                     <img src='/portfolio_logo.svg' alt='Logo' className='logo'/>
                 </div>
-                <h3 className='about'>About</h3>
+                <div className='about' onClick={handleAboutClick}>
+                    <h3>Alex Go</h3>
+                    <h6>Full Stack Developer</h6>
+                </div>
             </div>
         </div>
     )

ad827fe8aaa30e613c72443aab55b0b7fb5039c0
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2024-11-11 23:34:07 -0700

    fixed back button

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -10,10 +10,8 @@ function AboutPage() {
 
     return (
         <div className='about-page'>
-            <div className='top'>
-                <div className='back-button' onClick={handleBack}>
-                    <i className="fa-solid fa-arrow-left"></i><span>Back</span>
-                </div>
+            <div className='back-button' onClick={handleBack}>
+                <i className="fa-solid fa-arrow-left"></i><span>Back</span>
             </div>
             <div className='content'>
                 <h1>Undergoing a design process...</h1>

b1270da339327715f3fb37a5a8efe7bcdc4e71ed
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2025-01-16 21:41:10 -0700

    renamed machine-demo page

--- src/router.js
+++ src/router.js
@@ -5,7 +5,7 @@ import Riddym from "./components/Riddym/index.js";
 import Babbl from './components/Babbl/index.js';
 import Fortbnb from './components/Fortbnb/index.js';
 import AboutPage from "./components/AboutPage/index.js";
-import MachineDemo from "./components/machine-demo/index.js"
+import MachineDemo from "./components/MachineDemo/index.js"
 
 const router = createBrowserRouter([
     {

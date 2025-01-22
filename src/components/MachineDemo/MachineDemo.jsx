8d3aac23f714e88f38fbc417c079e6050eece23b
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2025-01-21 21:34:10 -0700

    imported components

--- src/components/MachineDemo/MachineDemo.jsx
+++ src/components/MachineDemo/MachineDemo.jsx
@@ -1,12 +1,22 @@
 import Machines from '../Machines';
 import MachineDashboard from '../MachineDashboard';
 import './MachineDemo.css';
+import { useEffect } from 'react';
 
 function MachineDemo() {
+
+    // keep page static
+    useEffect(() => {
+        document.body.style.overflow = "hidden";
+
+        return () => {
+            document.body.style.overflow = "auto";
+        };
+    }, []);
+
     return (
         <div className='machine-demo-page'>
-            <div className='nav-bar'>
-            </div>
+            <div className='nav-bar'></div>
             <div className='content'>
                 <MachineDashboard />
                 <Machines />

b97d0fb0d5d561e77775dd6f4b61b472916634ce
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2025-01-23 23:38:45 -0700

    added not-working tool tips

--- src/components/MachineDemo/MachineDemo.jsx
+++ src/components/MachineDemo/MachineDemo.jsx
@@ -18,12 +18,22 @@ function MachineDemo() {
         <div className='machine-demo-page'>
             <div className='nav-bar'>
                 <div className='logo-container'>
-                    <img src='/handwrytten_logo.png' alt='riddym' loading="lazy"/>
+                    <img src='/handwrytten_logo_2.png' alt='riddym' loading="lazy"/>
                 </div>
+                <span></span>
                 <div className='options'>
-                    <i class="fa-solid fa-gauge"></i>
-                    <i class="fa-solid fa-print"></i>
-                    <i class="fa-solid fa-bars-progress"></i>
+                    <div className='option-item'>
+                        <i className="fa-solid fa-gauge"></i>
+                        <span className='tooltip'>Dashboard</span>
+                    </div>
+                    <div className='option-item'>
+                        <i className="fa-solid fa-print"></i>
+                        <span className='tooltip'>Machines</span>
+                    </div>
+                    <div className='option-item'>
+                        <i className="fa-solid fa-bars-progress"></i>
+                        <span className='tooltip'>Jobs</span>
+                    </div>
                 </div>
             </div>
             <div className='content'>

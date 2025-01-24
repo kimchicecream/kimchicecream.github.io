0d4fde3490e42f460bb6b0a6302362062bfb5209
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2025-01-23 23:04:32 -0700

    added nav bar buttons

--- src/components/MachineDemo/MachineDemo.jsx
+++ src/components/MachineDemo/MachineDemo.jsx
@@ -16,7 +16,16 @@ function MachineDemo() {
 
     return (
         <div className='machine-demo-page'>
-            <div className='nav-bar'></div>
+            <div className='nav-bar'>
+                <div className='logo-container'>
+                    <img src='/handwrytten_logo.png' alt='riddym' loading="lazy"/>
+                </div>
+                <div className='options'>
+                    <i class="fa-solid fa-gauge"></i>
+                    <i class="fa-solid fa-print"></i>
+                    <i class="fa-solid fa-bars-progress"></i>
+                </div>
+            </div>
             <div className='content'>
                 <MachineDashboard />
                 <Machines />

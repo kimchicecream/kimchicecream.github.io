389b092654366e2932f4046880e95a536c574ac0
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2025-01-26 18:06:43 -0700

    configured frontend to fetch backend data

--- src/components/MachineDashboard/MachineDashboard.jsx
+++ src/components/MachineDashboard/MachineDashboard.jsx
@@ -1,8 +1,31 @@
+import { useState, useEffect } from 'react';
 import './MachineDashboard.css';
 
 function MachineDashboard() {
+    const [numbers, setNumbers] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await fetch('https://kimchicecream-github-io.onrender.com/api/scape-performance');
+                const data = await response.json();
+                setNumbers(data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        }
+
+        fetchData();
+    }, []);
+
     return (
         <div className='machine-dashboard'>
+            <h1>Performance Data</h1>
+            <ul>
+                {numbers.map((num, index) => (
+                    <li key={index}>{num}</li>
+                ))}
+            </ul>
         </div>
     )
 };

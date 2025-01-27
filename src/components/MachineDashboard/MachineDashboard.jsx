358ba95cf80bf038403ed926fef7c325163514cb
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2025-01-26 22:00:16 -0700

    some CSS magic

--- src/components/MachineDashboard/MachineDashboard.jsx
+++ src/components/MachineDashboard/MachineDashboard.jsx
@@ -7,11 +7,14 @@ function MachineDashboard() {
     useEffect(() => {
         async function fetchData() {
             try {
-                // LIVE
-                const response = await fetch('https://kimchicecream-github-io.onrender.com/api/scrape-performance');
-                // LOCAL
-                // const response = await fetch('http://localhost:5001/api/scrape-performance');
+                // Uncomment before pushing changes
+                // const response = await fetch('https://kimchicecream-github-io.onrender.com/api/scrape-performance');
+
+                // Uncomment for local tests
+                const response = await fetch('http://localhost:5001/api/scrape-performance');
+
                 const data = await response.json();
+                // const filteredData = data.filter((_, index) => index !== 1 && index !== 3);
                 setNumbers(data);
             } catch (error) {
                 console.error('Error fetching data:', error);
@@ -23,12 +26,34 @@ function MachineDashboard() {
 
     return (
         <div className='machine-dashboard'>
-            <h1>Performance Data</h1>
-            <ul>
-                {numbers.map((num, index) => (
-                    <li key={index}>{num}</li>
-                ))}
-            </ul>
+            <div className='title'>
+                <h1>Dashboard</h1>
+            </div>
+            <h3>Performance Data</h3>
+            <div className='total-stats'>
+                <div className='container'>
+                    {numbers.length > 0 && (
+                        <div className='total-orders'>{numbers[0]}</div>
+                    )}
+                    <div className='info'>
+                        <p>Total open orders</p><i class="fa-solid fa-circle-info"></i>
+                    </div>
+                    <div className='tooltip'>
+                        The total number of orders that have not been pulled.
+                    </div>
+                </div>
+                <div className='container'>
+                    {numbers.length > 2 && (
+                        <div className='total-orders'>{numbers[2]}</div>
+                    )}
+                    <div className='info'>
+                        <p>Orders completed today</p><i class="fa-solid fa-circle-info"></i>
+                    </div>
+                    <div className='tooltip'>
+                        The total number of orders fulfilled.
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };

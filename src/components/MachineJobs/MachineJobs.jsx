2e4a29e0bd1729cc9960c918c1ddda452c517f5e
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2025-01-30 19:59:12 -0700

    restructured MachineJobs layout

--- src/components/MachineJobs/MachineJobs.jsx
+++ src/components/MachineJobs/MachineJobs.jsx
@@ -7,6 +7,19 @@ function MachineJobs() {
     const [jobData, setJobData] = useState([]);
     const [loading, setLoading] = useState(false);
 
+    // const machinesColumn1 = Array.from({ length: (90 - 72) / 2 + 1 }, (_, i) => 72 + i * 2); // even: 72-90
+    // const machinesColumn2 = Array.from({ length: (89 - 71) / 2 + 1 }, (_, i) => 71 + i * 2); // odd: 71-89
+    // const machinesColumn3 = Array.from({ length: (109 - 91) / 2 + 1 }, (_, i) => 91 + i * 2); // odd: 91-109
+    // const machinesColumn4 = Array.from({ length: (110 - 92) / 2 + 1 }, (_, i) => 92 + i * 2); // even: 92-110
+
+    useEffect(() => {
+        document.body.style.overflow = "hidden";
+
+        return () => {
+            document.body.style.overflow = "auto";
+        };
+    }, []);
+
     async function fetchJobData(machine) {
         setLoading(true);
         setSelectedMachine(machine);
@@ -25,18 +38,31 @@ function MachineJobs() {
         } catch (error) {
             console.error('Error fetching machine data:', error);
             setJobData([]); // clear job data if request fails
+        } finally {
+            setLoading(false);
         }
-        setLoading(false);
     }
 
     return (
         <div className='machine-jobs-container'>
-            <div className='title'>
+            {/* <div className='title'>
                 <h1>Jobs</h1>
+            </div> */}
+            <div className='general-info'>
+                <div className='left-column'>
+                    <div className='big-box' id='1'></div>
+                    <div className='big-box' id='2'></div>
+                </div>
+                <div className='right-column'>
+                    <div className='small-box' id='1'></div>
+                    <div className='small-box' id='2'></div>
+                    <div className='small-box' id='3'></div>
+                    <div className='small-box' id='4'></div>
+                </div>
             </div>
             <div className='machines-module'>
                 <div className='machine-selector'>
-                    <div className='machine-list'>
+                <div className='machine-list'>
                         {machines.map((machine) => (
                             <button
                                 key={machine}

f2a680e2220ac02de6305f5a2470d7fafae7f43e
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2025-07-21 06:13:27 -0700

    style: began new styling

---
+++ src/pages/New/New.jsx
@@ -0,0 +1,12 @@
+import "./New.css";
+
+function New() {
+    return (
+        <div className="homepage">
+            <div className="sticky-profile"></div>
+            <div className="contents-section"></div>
+        </div>
+    )
+}
+
+export default New;
--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import Riddym from "./components/Riddym/index.js";
 import Babbl from './components/Babbl/index.js';
 import Fortbnb from './components/Fortbnb/index.js';
 import AboutPage from "./components/AboutPage/index.js";
+import New from "./pages/New/index.js";
 
 const router = createBrowserRouter([
     {
@@ -31,6 +32,10 @@ const router = createBrowserRouter([
         {
           path: "/about",
           element: <AboutPage />
+        },
+        {
+          path: "/new",
+          element: <New />
         }
       ]
     }

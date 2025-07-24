07402319dbe94e913c31cfda2b5a60cc72488ac7
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2025-07-24 05:00:02 -0700

    styling: implemented transition animations with media queries

--- src/pages/New/New.jsx
+++ src/pages/New/New.jsx
@@ -15,9 +15,9 @@ function New() {
                 <span></span>
                 <div className="info-bite-container">
                     <InformationBite id='info-one' icon={<LuMapPin />} title="LOCATION" option="Tempe, AZ, USA" />
-                    <InformationBite id='info-one' icon={<LuMail />} title="EMAIL" option="gohalexx@gmail.com" />
-                    <InformationBite id='info-one' icon={<LuGithub />} title="GITHUB" option={<a href="https://github.com/kimchicecream" target="_blank" rel="noopener noreferrer">@kimchicecream</a>} />
-                    <InformationBite id='info-one' icon={<LuLinkedin />} title="LINKEDIN" option={<a href="https://www.linkedin.com/in/alexxgo" target="_blank" rel="noopener noreferrer">in/alexxgo</a>} />
+                    <InformationBite id='info-two' icon={<LuMail />} title="EMAIL" option="gohalexx@gmail.com" />
+                    <InformationBite id='info-three' icon={<LuGithub />} title="GITHUB" option={<a href="https://github.com/kimchicecream" target="_blank" rel="noopener noreferrer">@kimchicecream</a>} />
+                    <InformationBite id='info-four' icon={<LuLinkedin />} title="LINKEDIN" option={<a href="https://www.linkedin.com/in/alexxgo" target="_blank" rel="noopener noreferrer">in/alexxgo</a>} />
                 </div>
                 <span></span>
                 <div className="sticky-footer">

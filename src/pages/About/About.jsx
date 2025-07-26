02c5fbd3a903bd65537b460a3ba7c0790e5769ea
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2025-07-26 11:53:24 -0700

    added introduction paragraph

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -5,7 +5,12 @@ function About() {
     return (
         <>
             <div className='about-container'>
-                <PageTitle pageTitle='About Alex' />
+                <PageTitle pageTitle='About Alex 👨🏻‍💻' />
+                <div className='intro-block'>
+                    <p>
+                        My name is Alex, a software engineer who traded the beaches of Guam for the hot and dry Arizona desert. I specialize in full-stack web development and intuitive UI/UX design.
+                    </p>
+                </div>
             </div>
         </>
     );

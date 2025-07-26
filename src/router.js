0fd8efc653d82235dafa92bee2d0cae505ea5628
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2025-07-25 20:07:27 -0700

    deprecated: moved every old components into a deprecated folder

--- src/router.js
+++ src/router.js
@@ -1,12 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
-import Layout from './components/Layout.js';
 import Homepage from './pages/Homepage/index.js';
 
 const router = createBrowserRouter([
     {
       // Root route
       path: "/",
-      element: <Layout />,
       children: [
         {
           path: "/",

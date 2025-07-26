21a7c3fdf7b398acb48b4153a6045f33661ddb81
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2025-07-25 19:41:22 -0700

    rename New components to Homepage and delete old

--- src/router.js
+++ src/router.js
@@ -1,11 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from './components/Layout.js';
-import Homepage from './components/Homepage';
+import Homepage from './pages/Homepage/index.js';
 import Riddym from "./components/Riddym/index.js";
 import Babbl from './components/Babbl/index.js';
 import Fortbnb from './components/Fortbnb/index.js';
 import AboutPage from "./components/AboutPage/index.js";
-import New from "./pages/New/index.js";
 
 const router = createBrowserRouter([
     {
@@ -32,10 +31,6 @@ const router = createBrowserRouter([
         {
           path: "/about",
           element: <AboutPage />
-        },
-        {
-          path: "/new",
-          element: <New />
         }
       ]
     }

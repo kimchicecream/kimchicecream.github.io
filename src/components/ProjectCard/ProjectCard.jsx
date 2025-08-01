4a10de1c43f85a1eb1a69e17d8e85c1df2877120
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2025-07-31 18:46:45 -0700

    completed ProjectCard component layout

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,8 +1,23 @@
+import TechBox from '../TechBox/TechBox';
 import './ProjectCard.css';
 
 function ProjectCard() {
     return (
-        <></>
+        <>
+            <div className='project-card-container'>
+                <div className='img'></div>
+                <div className='desc'>
+                    <span id='timeframe'></span>
+                    <div className='title-category'>
+                        <h3 id='title'></h3>
+                        <span id='category'></span>
+                    </div>
+                    <div className='project-techs'>
+                        <TechBox />
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,7 +1,15 @@
+import PageTitle from '../../components/PageTitle';
 import './Blog.css';
 
 function Blog() {
-
+    return (
+        <>
+            <div className='blog-container'>
+                <PageTitle pageTitle="Alex's Blog" />
+                <span id='marker'></span>
+            </div>
+        </>
+    )
 };
 
 export default Blog;

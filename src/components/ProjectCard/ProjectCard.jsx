cb5896403f1e8683eb7c008aab9939339e9569ce
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2025-08-01 08:58:00 -0700

    completed styling for ProjectCard component

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -4,13 +4,15 @@ function ProjectCard({ imageSrc, timeframe, title, category, tech }) {
     return (
         <>
             <div className='project-card-container'>
-                <div className='project-img'>{imageSrc}</div>
+                <div className='project-img'>
+                    <img src={imageSrc} />
+                </div>
                 <div className='desc'>
-                    <span id='timeframe'>{timeframe}</span>
-                    <div className='title-category'>
-                        <h3 id='title'>{title}</h3>
+                    <div className='timeframe-category'>
+                        <span id='timeframe'>{timeframe}</span>
                         <span id='category'>{category}</span>
                     </div>
+                    <h3 id='title'>{title}</h3>
                     <div className='project-techs'>{tech}</div>
                 </div>
             </div>
